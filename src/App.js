793b0e88b41284b47c71711898901e073ee98e2e
Eko Firlianto <eko.frilianto@gmail.com>
2022-04-27 16:35:01 +0700

    make it better

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          Sudah Ditambahkan
+          <br />
+          By Eko Firlianto
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
